3fbaeb74900f5f0fe551929565f93ef727074499
LucasSpinelli003 <rm97757@fiap.com.br>
2024-02-05 17:23:16 -0300

    feat - initial commit - transaction page and components

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,34 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionContainer, TrasactionsTable } from "./styles";
+
+export function Transaction (){
+    return( 
+    <div>
+        <Header />
+        <Summary />
+
+
+        <TransactionContainer>
+        <SearchForm />
+            <TrasactionsTable>
+                <tbody>
+                    <tr>
+                        <td width="50%">Desenvolvimento de site</td>
+                        <td> <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+                        <td>Venda</td>
+                        <td>13/04/2022</td>
+                    </tr>
+                    <tr>
+                        <td width="50%">Hamburguer</td>
+                        <td> <PriceHighlight variant="outcome">- R$ 70,00</PriceHighlight></td>
+                        <td>Venda</td>
+                        <td>10/04/2022</td>
+                    </tr>
+                </tbody>
+            </TrasactionsTable>
+        </TransactionContainer>
+    </div>
+    )
+}
