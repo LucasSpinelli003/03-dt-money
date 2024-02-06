222fc52776fe817e0c0e52ab5c720efc2508f3b3
LucasSpinelli003 <rm97757@fiap.com.br>
2024-02-06 18:09:55 -0300

    [Feat] - api add

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Overlay, Content } from "./styles";
+import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
     return (
@@ -9,7 +10,30 @@ export function NewTransactionModal() {
             <Content>
                 <Dialog.Title>Nova Transação</Dialog.Title>
 
-                <Dialog.Close />    
+                <CloseButton>
+                    <X size={24}/>
+                </CloseButton>    
+                <form action="">
+                    <input type="text" placeholder="descrição" required />
+                    <input type="number" placeholder="Preço" required />
+                    <input type="text" placeholder="Categoria" required />
+
+                    <TransactionType>
+                        <TransactionTypeButton variant="income">
+                            <ArrowCircleUp size={24}/>
+                            Entrada
+                        </TransactionTypeButton>
+                        <TransactionTypeButton variant="outcome">
+                            <ArrowCircleDown size={24}/>
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
+
+                    <button type="submit">
+                        Cadastrar
+                    </button>
+                </form>
+
             </Content>
         </Dialog.Portal>
     )
